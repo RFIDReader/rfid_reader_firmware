a619d6fa14537ff74f4e691f1fc46e2d16659eb7
Pratham Jaiswal <pr47h4m@gmail.com>
2024-06-14 16:12:59 +0530

    Part of buzzer

---
+++ main/prphs/melodies.h
@@ -0,0 +1,17 @@
+//
+// Created by Pratham Jaiswal on 10/04/24.
+//
+
+#ifndef RFID_READER_MELODIES_H
+#define RFID_READER_MELODIES_H
+
+//#include <stdint.h>
+//#include "notes.h"
+
+//const uint16_t mario_melody_length = 16;
+//const uint16_t mario_melody_notes[] = {E4, E4, REST, E4, REST, C4, E4, REST, G4, REST, REST, REST, G3, REST, REST,
+//                                       REST};
+//const uint32_t mario_melody_durations[] = {125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125,
+//                                           125};
+
+#endif //RFID_READER_MELODIES_H
---
+++ main/prphs/notes.h
@@ -0,0 +1,27 @@
+//
+// Created by Pratham Jaiswal on 10/04/24.
+//
+
+#ifndef RFID_READER_NOTES_H
+#define RFID_READER_NOTES_H
+
+#define REST 0
+#define B3 246
+#define C4 261
+#define D4 293
+#define E4 329
+#define F4 349
+#define G4 392
+#define A4 440
+#define B4 493
+#define C5 523
+#define D5 587
+#define E5 659
+#define F5 698
+#define G5 783
+#define A5 880
+#define B5 987
+#define C6 1046
+#define G3 196
+
+#endif //RFID_READER_NOTES_H
