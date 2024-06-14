d4425fcfdc5a8bddb6fe7af7452a41e412eaa314
Pratham Jaiswal <pr47h4m@gmail.com>
2024-06-14 16:12:10 +0530

    OTA

---
+++ main/ota/ota.c
@@ -0,0 +1,5 @@
+//
+// Created by Pratham Jaiswal on 20/03/24.
+//
+
+#include "ota.h"
---
+++ main/ota/ota.h
@@ -0,0 +1,20 @@
+//
+// Created by Pratham Jaiswal on 20/03/24.
+//
+
+#ifndef RFID_READER_OTA_H
+#define RFID_READER_OTA_H
+
+#ifdef CONFIG_IDF_TARGET_ESP32S3
+
+
+
+#elif defined(CONFIG_IDF_TARGET_ESP32)
+
+
+
+#endif
+
+void ota_start(char *version);
+
+#endif //RFID_READER_OTA_H
